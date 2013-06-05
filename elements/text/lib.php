b58b24c2db3497624c2040755224e57efdc7557c
Mark Nelson <markn@moodle.com>
2013-06-05 17:48:17 +0800

    Introduced the text element

---
+++ elements/text/lib.php
@@ -0,0 +1,66 @@
+<?php
+
+// This file is part of the customcert module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The text elements core interaction API.
+ *
+ * @package    customcertelement_text
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
+
+require_once($CFG->dirroot . '/mod/customcert/elements/element.class.php');
+
+class customcert_element_text extends customcert_element_base {
+
+    /**
+     * This function renders the form elements when adding a customcert element.
+     *
+     * @param stdClass $mform the edit_form instance.
+     */
+    public function render_form_elements($mform) {
+        $mform->addElement('textarea', 'text_' . $this->element->id, get_string('text', 'customcertelement_text'));
+        $mform->setType('text_' . $this->element->id, PARAM_RAW);
+        $mform->setDefault('text_' . $this->element->id, $this->element->data);
+        $mform->addHelpButton('text_' . $this->element->id, 'text', 'customcertelement_text');
+
+        parent::render_form_elements($mform);
+    }
+
+    /**
+     * This will handle how form data will be saved into the data column in the
+     * customcert_elements table.
+     *
+     * @param stdClass $data the form data.
+     * @return string the text
+     */
+    public function save_unique_data($data) {
+        $text = 'text_' . $this->element->id;
+        return $data->$text;
+    }
+
+    /**
+     * Handles rendering the element on the pdf.
+     *
+     * @param stdClass $pdf the pdf object
+     */
+    public function render($pdf) {
+        parent::render_content($pdf, $this->element->data);
+    }
+}
