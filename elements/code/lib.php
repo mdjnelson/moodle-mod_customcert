659719945d46387ca8823ae45c51df97298a593d
Mark Nelson <markn@moodle.com>
2013-06-05 16:23:11 +0800

    Introduced the code element

---
+++ elements/code/lib.php
@@ -0,0 +1,48 @@
+<?php
+
+// This file is part of the customcert module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle. If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * The code elements core interaction API.
+ *
+ * @package    customcertelement_code
+ * @copyright  Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
+
+require_once($CFG->dirroot . '/mod/customcert/elements/element.class.php');
+
+class customcert_element_code extends customcert_element_base {
+
+    /**
+     * Handles rendering the element on the pdf.
+     *
+     * @param stdClass $pdf the pdf object
+     * @param int $userid
+     */
+    public function render($pdf, $userid) {
+        global $DB;
+
+        // Get the page.
+        $page = $DB->get_record('customcert_pages', array('id' => $this->element->pageid), '*', MUST_EXIST);
+        // Now we can get the issue for this user.
+        $issue = $DB->get_record('customcert_issues', array('userid' => $userid, 'customcertid' => $page->customcertid), '*', MUST_EXIST);
+
+        parent::render_content($pdf, $issue->code);
+    }
+}
