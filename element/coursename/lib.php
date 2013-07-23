50505d81960e7a39a81cf07b1ef3045ba0f0676f
Mark Nelson <markn@moodle.com>
2013-07-23 18:39:58 +0800

    Introduced the course name element

---
+++ element/coursename/lib.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of the customcert module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
+
+require_once($CFG->dirroot . '/mod/customcert/element/element.class.php');
+
+/**
+ * The customcert element coursename's core interaction API.
+ *
+ * @package    customcertelement_coursename
+ * @copyright  2013 Mark Nelson <markn@moodle.com>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class customcert_element_coursename extends customcert_element_base {
+
+    /**
+     * Handles rendering the element on the pdf.
+     *
+     * @param pdf $pdf the pdf object
+     * @param bool $preview true if it is a preview, false otherwise
+     */
+    public function render($pdf, $preview) {
+        global $COURSE;
+
+        parent::render_content($pdf, $COURSE->fullname);
+    }
+}
