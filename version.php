7a0030d0e764e7068dac6eb847506ea8bef4b08e
Mark Nelson <markn@moodle.com>
2013-10-31 14:53:11 +0800

    Increased version required

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 $module->version   = 2013072600; // The current module version (Date: YYYYMMDDXX).
-$module->requires  = 2012120300; // Requires this Moodle version.
+$module->requires  = 2013101800; // Requires this Moodle version (2.6 beta).
 $module->cron      = 0; // Period for cron to check this module (secs).
 $module->component = 'mod_customcert';
 
