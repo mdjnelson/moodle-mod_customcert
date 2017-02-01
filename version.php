ca08fe954a4a70ffed0cfedf895984b59f99fe12
Mark Nelson <markn@moodle.com>
2017-02-01 18:23:15 +0800

    #51 Added ability to verify certificates

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
-$plugin->version   = 2016120503; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2016120504; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2016120500; // Requires this Moodle version (3.2).
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_customcert';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "3.2 release (Build: 2016120502)"; // User-friendly version number.
+$plugin->release   = "3.2 release (Build: 2016120504)"; // User-friendly version number.
