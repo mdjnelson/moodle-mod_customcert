8e11a0fe3accdd7e99db92295a59bcb21f69bb38
Mark Nelson <markn@moodle.com>
2018-02-22 18:49:48 +0800

    #166 Fixed encoding content links

--- backup/moodle2/backup_customcert_activity_task.class.php
+++ backup/moodle2/backup_customcert_activity_task.class.php
@@ -63,11 +63,11 @@ class backup_customcert_activity_task extends backup_activity_task {
 
         // Link to the list of customcerts.
         $search = "/(".$base."\/mod\/customcert\/index.php\?id\=)([0-9]+)/";
-        $content = preg_replace($search, '$@customcertINDEX*$2@$', $content);
+        $content = preg_replace($search, '$@CUSTOMCERTINDEX*$2@$', $content);
 
         // Link to customcert view by moduleid.
         $search = "/(".$base."\/mod\/customcert\/view.php\?id\=)([0-9]+)/";
-        $content = preg_replace($search, '$@customcertVIEWBYID*$2@$', $content);
+        $content = preg_replace($search, '$@CUSTOMCERTVIEWBYID*$2@$', $content);
 
         return $content;
     }
