c61e6870802fa80d9449ddb68fe812b83973a048
Mark Nelson <markn@moodle.com>
2018-05-26 17:01:01 +0800

    Fixed misleading comment in version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 $plugin->version   = 2018051700; // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700; // Requires this Moodle version (3.4).
+$plugin->requires  = 2018051700; // Requires this Moodle version (3.5).
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_customcert';
 
