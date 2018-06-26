9d27015a5fdda4488513a82d6987b3feedf43b97
Mark Nelson <markn@moodle.com>
2018-06-26 17:14:42 +0800

    #203 Removed 'code' column
    Not all certificates contain a 'code' element,
    making this column unnecessary. It may also
    confuse users when it is not applicable. For
    those who are interested they can always use
    the 'verify' functionality to confirm
    certificates, rather than using this page.

--- classes/report_table.php
+++ classes/report_table.php
@@ -67,13 +67,11 @@ class report_table extends \table_sql {
 
         $columns = array(
             'fullname',
-            'timecreated',
-            'code'
+            'timecreated'
         );
         $headers = array(
             get_string('fullname'),
-            get_string('receiveddate', 'customcert'),
-            get_string('code', 'customcert')
+            get_string('receiveddate', 'customcert')
         );
 
         // Check if we were passed a filename, which means we want to download it.
@@ -95,7 +93,6 @@ class report_table extends \table_sql {
         $this->define_headers($headers);
         $this->collapsible(false);
         $this->sortable(true);
-        $this->no_sorting('code');
         $this->no_sorting('download');
         $this->is_downloadable(true);
 
@@ -128,16 +125,6 @@ class report_table extends \table_sql {
      */
     public function col_timecreated($user) {
         return userdate($user->timecreated);
-    }
-
-    /**
-     * Generate the code column.
-     *
-     * @param \stdClass $user
-     * @return string
-     */
-    public function col_code($user) {
-        return $user->code;
     }
 
     /**
