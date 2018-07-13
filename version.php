13c1998c494788dab253aadb100b44d4b5d2d87b
Mark Nelson <markn@moodle.com>
2018-07-13 22:40:55 +0800

    Bumped version

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
-$plugin->version   = 2018051703; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2018051704; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018051700; // Requires this Moodle version (3.5).
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_customcert';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "3.5.3"; // User-friendly version number.
+$plugin->release   = "3.5.4"; // User-friendly version number.
