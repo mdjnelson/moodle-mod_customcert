10fcb5a342fb5b931bf4c47513c39d51c71651dc
Mark Nelson <markn@moodle.com>
2018-07-28 11:03:49 +0800

    Fixed misleading version number in release comment

--- element/categoryname/version.php
+++ element/categoryname/version.php
@@ -25,5 +25,5 @@
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
 $plugin->version   = 2018051700; // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700; // Requires this Moodle version (3.4).
+$plugin->requires  = 2018051700; // Requires this Moodle version (3.5).
 $plugin->component = 'customcertelement_categoryname';
