322abdbba33027ce37768bb5595bddc60e3218fa
Nicolas Roeser <nicolas.roeser@uni-ulm.de>
2018-11-06 14:53:06 +0100

    Add vertical spacing after certificate download button
    Increase the vertical spacing after the single button for downloading
    user’s own certificate. This makes the page look better.

--- view.php
+++ view.php
@@ -137,6 +137,7 @@ if (!$downloadown && !$downloadissue) {
     $linkname = get_string('getcustomcert', 'customcert');
     $link = new moodle_url('/mod/customcert/view.php', array('id' => $cm->id, 'downloadown' => true));
     $downloadbutton = new single_button($link, $linkname, 'post', true);
+    $downloadbutton->class .= ' m-b-1';  // Seems a bit hackish, ahem.
     $downloadbutton = $OUTPUT->render($downloadbutton);
 
     // Output all the page data.
