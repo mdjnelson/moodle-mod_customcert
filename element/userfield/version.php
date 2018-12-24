b211a06bd50559c3e6c1a6d36600307084fa6e5b
Mark Nelson <markn@moodle.com>
2018-12-24 15:50:40 +0800

    Bumped version for 3.6

--- element/userfield/version.php
+++ element/userfield/version.php
@@ -24,6 +24,6 @@
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
-$plugin->version   = 2018051700; // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2018051700; // Requires this Moodle version (3.5).
+$plugin->version   = 2018120300; // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2018120300; // Requires this Moodle version (3.5).
 $plugin->component = 'customcertelement_userfield';
