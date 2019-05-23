64104a3063b3c995ee1390d9129a34c4d325b51d
Dustin Brisebois <dustin.brisebois@lambdasolutions.net>
2019-05-23 14:03:39 -0700

    add viewnavigation permission (for managing profile viewnavigation accessibility)

--- db/access.php
+++ db/access.php
@@ -70,6 +70,17 @@ $capabilities = array(
             'manager' => CAP_ALLOW
         )
     ),
+    
+    'mod/customcert:viewnavigation' => array(
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_MODULE,
+        'archetypes' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
 
     'mod/customcert:viewallcertificates' => array(
         'captype' => 'read',
