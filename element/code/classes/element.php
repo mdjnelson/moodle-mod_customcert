c69d4822ccc69aaa3a68f4392c1b1730a29896a5
Mark Nelson <mdjnelson@gmail.com>
2020-08-08 15:50:14 +0200

    Ignore multiple certificate codes (#363)

--- element/code/classes/element.php
+++ element/code/classes/element.php
@@ -54,7 +54,7 @@ class element extends \mod_customcert\element {
             $customcert = $DB->get_record('customcert', array('templateid' => $page->templateid), '*', MUST_EXIST);
             // Now we can get the issue for this user.
             $issue = $DB->get_record('customcert_issues', array('userid' => $user->id, 'customcertid' => $customcert->id),
-                '*', MUST_EXIST);
+                '*', IGNORE_MULTIPLE);
             $code = $issue->code;
         }
 
