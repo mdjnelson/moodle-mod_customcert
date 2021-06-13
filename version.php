26802e9262c4ba0bf7c3de29a2bc8cfe34026e07
Mark Nelson <mdjnelson@gmail.com>
2021-06-13 14:04:59 +0800

    Bump version

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
-$plugin->version   = 2021051700; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2021051701; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2021051700; // Requires this Moodle version (3.11).
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_customcert';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "3.11.0"; // User-friendly version number.
+$plugin->release   = "3.11.1"; // User-friendly version number.
