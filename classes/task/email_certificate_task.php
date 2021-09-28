0cecdd0ab54962feebd171353800ab1a419f0f5e
Mark Nelson <mdjnelson@gmail.com>
2021-09-28 22:07:21 +0800

    Do not encode html entities in emails (#457)

--- classes/task/email_certificate_task.php
+++ classes/task/email_certificate_task.php
@@ -211,7 +211,8 @@ class email_certificate_task extends \core\task\scheduled_task {
                     $subject = get_string('emailstudentsubject', 'customcert', $info);
                     $message = $textrenderer->render($renderable);
                     $messagehtml = $htmlrenderer->render($renderable);
-                    email_to_user($user, fullname($userfrom), $subject, $message, $messagehtml, $tempfile, $filename);
+                    email_to_user($user, fullname($userfrom), html_entity_decode($subject), $message, $messagehtml,
+                        $tempfile, $filename);
                 }
 
                 if ($customcert->emailteachers) {
@@ -222,8 +223,8 @@ class email_certificate_task extends \core\task\scheduled_task {
                     $message = $textrenderer->render($renderable);
                     $messagehtml = $htmlrenderer->render($renderable);
                     foreach ($teachers as $teacher) {
-                        email_to_user($teacher, fullname($userfrom), $subject, $message, $messagehtml, $tempfile,
-                            $filename);
+                        email_to_user($teacher, fullname($userfrom), html_entity_decode($subject), $message, $messagehtml,
+                            $tempfile, $filename);
                     }
                 }
 
@@ -242,8 +243,8 @@ class email_certificate_task extends \core\task\scheduled_task {
                             $emailuser = new \stdClass();
                             $emailuser->id = -1;
                             $emailuser->email = $email;
-                            email_to_user($emailuser, fullname($userfrom), $subject, $message, $messagehtml, $tempfile,
-                                $filename);
+                            email_to_user($emailuser, fullname($userfrom), html_entity_decode($subject), $message,
+                                $messagehtml, $tempfile, $filename);
                         }
                     }
                 }
