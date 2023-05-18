88f5712d165bb2cc3f29f4ca439f1e0c2176846c
Mark Nelson <mdjnelson@gmail.com>
2023-05-18 13:45:48 +0800

    Fix usage of deprecated 'cron_setup_user' function (#547)

--- classes/task/email_certificate_task.php
+++ classes/task/email_certificate_task.php
@@ -182,7 +182,7 @@ class email_certificate_task extends \core\task\scheduled_task {
             // Now, email the people we need to.
             foreach ($issuedusers as $user) {
                 // Set up the user.
-                cron_setup_user($user);
+                \core\cron::setup_user($user);
 
                 $userfullname = fullname($user);
                 $info->userfullname = $userfullname;
