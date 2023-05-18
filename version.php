dbd38229326f48b4fe32245f6e2c67e3ee7e038e
Mark Nelson <mdjnelson@gmail.com>
2023-05-18 12:23:22 +0800

    Bump version for 4.2

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
-$plugin->version   = 2022041903; // The current module version (Date: YYYYMMDDXX).
-$plugin->requires  = 2022041900; // Requires this Moodle version (4.0).
+$plugin->version   = 2023042400; // The current module version (Date: YYYYMMDDXX).
+$plugin->requires  = 2023042400; // Requires this Moodle version (4.2).
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_customcert';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "4.0.3"; // User-friendly version number.
+$plugin->release   = "4.2.0"; // User-friendly version number.
