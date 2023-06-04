e90690499639562e2e652f8a2ac9dd8203d8ef56
Mark Nelson <mdjnelson@gmail.com>
2023-06-05 06:35:30 +0800

    #518 Fix copyright

--- classes/event/template_deleted.php
+++ classes/event/template_deleted.php
@@ -18,7 +18,7 @@
  * Certificate template deleted event.
  *
  * @package   mod_customcert
- * @copyright 2017 Mark Nelson <markn@moodle.com>
+ * @copyright 2023 Leon Stringer <leon.stringer@ntlworld.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
@@ -30,7 +30,7 @@ use mod_customcert\template;
  * Certificate template deleted event class.
  *
  * @package   mod_customcert
- * @copyright 2017 Mark Nelson <markn@moodle.com>
+ * @copyright 2023 Leon Stringer <leon.stringer@ntlworld.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 class template_deleted extends \core\event\base {
