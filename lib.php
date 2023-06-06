mdjnelson/moodle-mod_customcert

commit 43646249eac7dff287b117bcbe249654ad03bb7b

Replaced prohibited logical operators

lib.php
@@ -233,7 +233,7 @@ function customcert_pluginfile($course, $cm, $context, $filearea, $args, $forced
         $fullpath = '/' . $context->id . '/mod_customcert/image/' . $relativepath;
 
         $fs = get_file_storage();
-        if (!$file = $fs->get_file_by_hash(sha1($fullpath)) or $file->is_directory()) {
+        if (!$file = $fs->get_file_by_hash(sha1($fullpath)) || $file->is_directory()) {
             return false;
         }
 
@@ -326,7 +326,7 @@ function customcert_extend_settings_navigation(settings_navigation $settings, na
     $keys = $customcertnode->get_children_key_list();
     $beforekey = null;
     $i = array_search('modedit', $keys);
-    if ($i === false and array_key_exists(0, $keys)) {
+    if ($i === false && array_key_exists(0, $keys)) {
         $beforekey = $keys[0];
     } else if (array_key_exists($i + 1, $keys)) {
         $beforekey = $keys[$i + 1];

classes/element_helper.php
@@ -76,7 +76,7 @@ class element_helper {
         $actualwidth = $pdf->GetStringWidth($content);
         $alignment = $element->get_alignment();
 
-        if ($w and $w < $actualwidth) {
+        if ($w && $w < $actualwidth) {
             $actualwidth = $w;
         }
 

classes/plugininfo/customcertelement.php
@@ -59,7 +59,7 @@ class customcertelement extends base {
             return;
         }
 
-        if (!$hassiteconfig or !file_exists($this->full_path('settings.php'))) {
+        if (!$hassiteconfig || !file_exists($this->full_path('settings.php'))) {
             return;
         }
 

commit 600d32887a10665018b8f0c8ec27f1db241b216d

#562 Fix images not showing on the reposition page
This was caused by 43646249eac7dff287b117bcbe249654ad03bb7b.

lib.php
@@ -233,7 +233,8 @@ function customcert_pluginfile($course, $cm, $context, $filearea, $args, $forced
         $fullpath = '/' . $context->id . '/mod_customcert/image/' . $relativepath;
 
         $fs = get_file_storage();
-        if (!$file = $fs->get_file_by_hash(sha1($fullpath)) || $file->is_directory()) {
+        $file = $fs->get_file_by_hash(sha1($fullpath));
+        if (!$file || $file->is_directory()) {
             return false;
         }
 