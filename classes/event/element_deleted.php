4adaa15ad9e239e095ce81ca3f74bcddbdb5e583
Mark Nelson <mdjnelson@gmail.com>
2023-06-10 06:57:46 +0800

    #564 Reword event descriptions

--- classes/event/element_deleted.php
+++ classes/event/element_deleted.php
@@ -50,10 +50,10 @@ class element_deleted extends \core\event\base {
     public function get_description() {
         if ($this->contextlevel == \context_system::instance()->contextlevel) {
             // If CONTEXT_SYSTEM assume it's a template.
-            return "The user with id '$this->userid' deleted an element with id '$this->objectid'.";
+            return "The user with id '$this->userid' deleted the element with id '$this->objectid'.";
         } else {
             // Else assume it's a module instance in a course.
-            return "The user with id '$this->userid' deleted an element with id '$this->objectid' in the certificate " .
+            return "The user with id '$this->userid' deleted the element with id '$this->objectid' in the certificate " .
                 "in course module '$this->contextinstanceid'.";
         }
     }
