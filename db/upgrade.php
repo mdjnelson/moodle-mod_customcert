9841f3cf353e0d54b7e56e72db66914b59545925
Mark Nelson <mdjnelson@gmail.com>
2024-08-31 13:09:36 +0100

    Removed unused 'customcert_email_task_prgrs' table (#531)

--- db/upgrade.php
+++ db/upgrade.php
@@ -286,5 +286,17 @@ function xmldb_customcert_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2022112810, 'customcert');
     }
 
+    if ($oldversion < 2022112812) {
+        // Drop unused table customcert_email_task_prgrs.
+        $table = new xmldb_table('customcert_email_task_prgrs');
+
+        if ($dbman->table_exists($table)) {
+            $dbman->drop_table($table);
+        }
+
+        // Customcert savepoint reached.
+        upgrade_mod_savepoint(true, 2022112812, 'customcert');
+    }
+
     return true;
 }
