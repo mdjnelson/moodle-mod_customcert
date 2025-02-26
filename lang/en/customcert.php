12463e5c66f983b3043e46cff86fcbc7f2a0092a
Raza403 <raza403@gmail.com>
2025-02-26 20:13:58 +0500

    customcert user's has option to use new or old format. Unique code is being generated. This PR is made for #1 and #2
    custom cert, updated the names of both code generation methods.
    
    Added view_user_cert page, a logged in user can see certificate if having a valid token and ecard code. This is being used from view_tokens.php page.
    
    customcert view certificate function is made. This is made for #212
    
    Updated verify certificate URL. This PR is made for 221
    
    Updated the verify URL for QR code.

--- lang/en/customcert.php
+++ lang/en/customcert.php
@@ -249,3 +249,7 @@ $string['verifycertificateanyone_help'] = 'This setting enables anyone with the 
 $string['verifycertificatedesc'] = 'This link will take you to a new screen where you will be able to verify certificates on the site';
 $string['width'] = 'Width';
 $string['width_help'] = 'This is the width of the certificate PDF in mm. For reference an A4 piece of paper is 210mm wide and a letter is 216mm wide.';
+$string['codegenerationmethod'] = 'Code generation method';
+$string['codegenerationmethod_desc'] = 'Choose between the two methods for generating certificate codes.';
+$string['Upper/lower/digits'] = '6aOdbLEuoC (Upper/lower/digits random string)';
+$string['digits-with-hyphens'] = '0123-4567-8901 (Digits with hyphens)';
