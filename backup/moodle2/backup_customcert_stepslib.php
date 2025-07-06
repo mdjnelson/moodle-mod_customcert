17461d7b61be80ba712841155f9804e937e140a6
Mark Nelson <mdjnelson@gmail.com>
2025-07-06 18:18:43 +0100

    Added missing fields to backups (#705)

--- backup/moodle2/backup_customcert_stepslib.php
+++ backup/moodle2/backup_customcert_stepslib.php
@@ -40,8 +40,9 @@ class backup_customcert_activity_structure_step extends backup_activity_structur
 
         // The instance.
         $customcert = new backup_nested_element('customcert', ['id'], [
-            'templateid', 'name', 'intro', 'introformat', 'requiredtime', 'verifyany', 'emailstudents',
-            'emailteachers', 'emailothers', 'protection', 'timecreated', 'timemodified']);
+            'templateid', 'name', 'intro', 'introformat', 'requiredtime', 'verifyany',
+            'deliveryoption', 'emailstudents', 'emailteachers', 'emailothers',
+            'protection', 'language', 'timecreated', 'timemodified']);
 
         // The template.
         $template = new backup_nested_element('template', ['id'], [
