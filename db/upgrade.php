313f290f4e2eff18b206d5bced21adb96f89746c
Mark Nelson <mdjnelson@gmail.com>
2025-07-06 14:44:26 +0100

    Fixed version bump issue (#684)

--- db/upgrade.php
+++ db/upgrade.php
@@ -316,7 +316,7 @@ function xmldb_customcert_upgrade($oldversion) {
         upgrade_plugin_savepoint(true, 2024042210, 'mod', 'customcert');
     }
 
-    if ($oldversion < 2025062100) {
+    if ($oldversion < 2024042213) {
         $table = new xmldb_table('customcert');
 
         // Add 'usecustomfilename' field.
@@ -332,7 +332,8 @@ function xmldb_customcert_upgrade($oldversion) {
         }
 
         // Savepoint reached.
-        upgrade_mod_savepoint(true, 2025062100, 'customcert');
+        upgrade_mod_savepoint(true, 2024042213, 'customcert');
     }
+
     return true;
 }
