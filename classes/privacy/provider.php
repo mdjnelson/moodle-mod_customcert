9b7051095ecb56d5287ee52129ef91b70db6b63d
Mark Nelson <mdjnelson@gmail.com>
2025-10-12 16:44:06 +0100

    Reorder implemented interfaces alphabetically in provider.php

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -40,8 +40,8 @@ use core_privacy\local\request\writer;
  */
 class provider implements
     \core_privacy\local\metadata\provider,
-    \core_privacy\local\request\plugin\provider,
-    \core_privacy\local\request\core_userlist_provider {
+    \core_privacy\local\request\core_userlist_provider,
+    \core_privacy\local\request\plugin\provider {
     /**
      * Return the fields which contain personal data.
      *
