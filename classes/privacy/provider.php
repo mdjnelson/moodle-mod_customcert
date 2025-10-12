8446e3f341d902e70feb6d2233eb5bbfccaeb100
Mark Nelson <mdjnelson@gmail.com>
2025-10-12 15:38:24 +0100

    Resolve line length warning in provider.php

--- classes/privacy/provider.php
+++ classes/privacy/provider.php
@@ -38,7 +38,10 @@ use core_privacy\local\request\writer;
  * @copyright  2018 Mark Nelson <markn@moodle.com>
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-class provider implements \core_privacy\local\request\core_userlist_provider, \core_privacy\local\metadata\provider, \core_privacy\local\request\plugin\provider {
+class provider implements
+    \core_privacy\local\metadata\provider,
+    \core_privacy\local\request\plugin\provider,
+    \core_privacy\local\request\core_userlist_provider {
     /**
      * Return the fields which contain personal data.
      *
