9929e5430c72daa7482db58138389afb5265dc58
Mark Nelson <mdjnelson@gmail.com>
2025-12-18 20:00:01 +0800

    Bumped version

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 
 defined('MOODLE_INTERNAL') || die('Direct access to this script is forbidden.');
 
-$plugin->version   = 2024042215; // The current module version (Date: YYYYMMDDXX).
+$plugin->version   = 2024042216; // The current module version (Date: YYYYMMDDXX).
 $plugin->requires  = 2024042200; // Requires this Moodle version (4.4).
 $plugin->cron      = 0; // Period for cron to check this module (secs).
 $plugin->component = 'mod_customcert';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = "4.4.7"; // User-friendly version number.
+$plugin->release   = "4.4.8"; // User-friendly version number.
