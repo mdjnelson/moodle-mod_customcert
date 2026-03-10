e2c2d60301d82090d20a41b916d2231e25c5f369
joerebbeck <joerebbeck@hotmail.com>
2026-03-10 22:08:06 +0100

    Add language strings for assignment feedback element

---
+++ element/assignfeedback/lang/en/customcertelement_assignfeedback.php
@@ -0,0 +1,35 @@
+<?php
+// This file is part of the customcert module for Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings for the assignfeedback customcert element.
+ *
+ * @package    customcertelement_assignfeedback
+ * @copyright  2026 Joe Rebbeck
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['pluginname'] = 'Assignment feedback';
+
+$string['assignment']      = 'Assignment';
+$string['assignment_help'] = 'Select the assignment whose feedback comments you want to display on the certificate. Only assignments within this course are listed.';
+
+$string['chooseassignment']      = 'Choose an assignment…';
+$string['noassignmentselected']  = 'Please select an assignment.';
+$string['nofeedbackavailable']   = 'No feedback available.';
+$string['previewtext']           = 'Great work — your analysis was thorough and well-structured.';
+
+$string['privacy:metadata'] = 'The Assignment feedback element plugin does not store any personal data itself. It reads feedback data already stored by the Assignment activity.';
