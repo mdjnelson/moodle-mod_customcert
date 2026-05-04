033c7d5152b1baa13ff59f4fe8850beeb7c71880
Mark Nelson <mdjnelson@gmail.com>
2026-05-04 19:09:04 +0100

    Add missing 'use context' import (#786)

--- element/qrcode/classes/element.php
+++ element/qrcode/classes/element.php
@@ -26,6 +26,7 @@ declare(strict_types=1);
 
 namespace customcertelement_qrcode;
 
+use context;
 use mod_customcert\element as base_element;
 use mod_customcert\element\constructable_element_interface;
 use mod_customcert\element\persistable_element_interface;
