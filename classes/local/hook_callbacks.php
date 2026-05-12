f59381b680478292259d521bbb3ae81729e6af60
Willian Mano <willianmanoaraujo@gmail.com>
2026-05-12 15:15:47 -0300

    Fixes #795 - Cannot compile closures which import variables using the use keyword

--- classes/local/hook_callbacks.php
+++ classes/local/hook_callbacks.php
@@ -51,12 +51,10 @@ class hook_callbacks {
      * @param di_configuration $config The DI configuration instance to register definitions in.
      */
     public static function di_configuration(di_configuration $config): void {
-        $logger = new template_logger();
-
         $config->add_definition(
             id: template_import_logger_interface::class,
-            definition: function () use ($logger): template_import_logger_interface {
-                return $logger;
+            definition: function () : template_import_logger_interface {
+                return new template_logger();
             }
         );
 
@@ -64,7 +62,8 @@ class hook_callbacks {
             id: template_appendix_manager_interface::class,
             definition: function (
                 clock $clock,
-            ) use ($logger): template_appendix_manager_interface {
+                template_import_logger_interface $logger,
+            ) : template_appendix_manager_interface {
                 $filemng = new template_appendix_manager();
                 $element = new element($clock, $logger, $filemng);
                 $filemng->set_element($element);
@@ -77,7 +76,8 @@ class hook_callbacks {
             definition: function (
                 template_appendix_manager_interface $filemng,
                 clock $clock,
-            ) use ($logger): template_file_manager_interface {
+                template_import_logger_interface $logger,
+            ) : template_file_manager_interface {
                 $element = new element($clock, $logger, $filemng);
                 $page = new page($clock, $element);
                 $tmpl = new template($clock, $page);
