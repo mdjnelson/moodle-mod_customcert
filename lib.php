d62b41a2378acfd3d92880e0e8f01f7e8cc4f851
Mark Nelson <mdjnelson@gmail.com>
2026-09-01 13:04:01 +0100

    Fix completionemailed never being evaluated by core (#645)
    customcert_get_coursemodule_info() was missing, so core completion never
    saw the rule as available and silently skipped it.

--- lib.php
+++ lib.php
@@ -289,6 +289,38 @@ function customcert_supports($feature) {
 }
 
 /**
+ * Populates the custom completion rules so core completion can evaluate
+ * mod_customcert\completion\custom_completion for this instance.
+ *
+ * Without this, cm_info::get_custom_data() never contains 'completionemailed', and core
+ * completion silently skips the rule instead of evaluating it.
+ *
+ * @param stdClass $coursemodule The coursemodule object (record).
+ * @return cached_cm_info|false
+ */
+function customcert_get_coursemodule_info($coursemodule) {
+    global $DB;
+
+    $customcert = $DB->get_record(
+        'customcert',
+        ['id' => $coursemodule->instance],
+        'id, name, completionemailed'
+    );
+    if (!$customcert) {
+        return false;
+    }
+
+    $info = new cached_cm_info();
+    $info->name = $customcert->name;
+
+    if ($coursemodule->completion == COMPLETION_TRACKING_AUTOMATIC) {
+        $info->customdata['customcompletionrules']['completionemailed'] = $customcert->completionemailed;
+    }
+
+    return $info;
+}
+
+/**
  * Used for course participation report (in case customcert is added).
  *
  * @return array
